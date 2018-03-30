ef59a8e6b936a9ad9e6cb170626d8a7b36c6e819
kguinto <klogold1@gmail.com>
2018-03-30 16:28:05 -0700

    Added option to set port through env variable

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const getRenderedComponent = require('./renderComponent');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
